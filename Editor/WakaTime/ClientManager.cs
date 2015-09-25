5e513a3c2a6c0bc63247ae1965f1df6eb6e80ed1
José Manuel Cámara Iglesias <josec89@gmail.com>
2015-09-25 09:32:43 +0200

    - Fixed clientmanager

--- Editor/WakaTime/ClientManager.cs
+++ Editor/WakaTime/ClientManager.cs
@@ -17,37 +17,37 @@ namespace WakaTime {
 			}
 		}
 
-		public static string GetClientPath () {
-			return Path.Combine (BaseDir, ClientPath);            
+		public static string GetClientPath() {
+			return Path.Combine(BaseDir, ClientPath);            
 		}
 
-		public static bool IsClientInstalled () {
-			return File.Exists (GetClientPath ());
+		public static bool IsClientInstalled() {
+			return File.Exists(GetClientPath());
 		}
-	
-		public static bool IsClientLatestVersion () {
-			if (!IsClientInstalled ()) {
+
+		public static bool IsClientLatestVersion() {
+			if (!IsClientInstalled()) {
 				return false;
 			}
 
 			return true;
 		}
 
-		public static void HeartBeat (string apiKey, string file, bool write = false) {
-			if (PythonManager.IsPythonInstalled ()) {
-				string arguments = "--key " + apiKey 
-					+ " --file " + file
-					+ " --plugin " + WakaTimeConstants.PLUGIN_NAME
-					+ " --project " + Main.GetProjectName ()
-					+ " --verbose";
+		public static void HeartBeat(string apiKey, string file, bool write = false) {
+			if (PythonManager.IsPythonInstalled()) {
+				string arguments = "--key " + apiKey
+				                   + " --file " + file
+				                   + " --plugin " + WakaTimeConstants.PLUGIN_NAME
+				                   + " --project " + Main.GetProjectName()
+				                   + " --verbose";
 
-				if(Main.IsDebug) {
-					UnityEngine.Debug.Log ("Sending file: " + PythonManager.GetPythonPath () + " " + GetClientPath () + " " + arguments);
+				if (Main.IsDebug) {
+					UnityEngine.Debug.Log("Sending file: " + PythonManager.GetPythonPath() + " " + GetClientPath() + " " + arguments);
 				}
 
-				Process p = new Process ();
-				p.StartInfo.FileName = PythonManager.GetPythonPath ();
-				p.StartInfo.Arguments = GetClientPath () + " " + arguments;    
+				Process p = new Process();
+				p.StartInfo.FileName = PythonManager.GetPythonPath();
+				p.StartInfo.Arguments = GetClientPath() + " " + arguments;    
 		
 				p.StartInfo.CreateNoWindow = true;
 				p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
@@ -57,16 +57,16 @@ namespace WakaTime {
 #if UNITY_EDITOR_WIN
 				p.StartInfo.UseShellExecute = true;
 #else
-				p.StartInfo.UseShellExecute = false;
+				p.StartInfo.UseShellExecute = true;
 #endif
 
 
-				p.Start ();
+				p.Start();
 
 //			UnityEngine.Debug.Log (p.StandardOutput.ReadToEnd ());
 //			UnityEngine.Debug.Log (p.StandardError.ReadToEnd ());
 
-				p.Close ();
+				p.Close();
 			}
 		}
 	}
